c91f0bc5ab6fdbb21d4835cd75e9f26aa9de9bba
Atishdhekne <157268153+Atishdhekne@users.noreply.github.com>
2026-06-12 14:11:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
+from streamlit.components.v1 import html
 import pathlib
 
 st.set_page_config(
     page_title="Patient Monitoring Prototype",
     page_icon="🏥",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
 html_content = pathlib.Path("index.html").read_text(encoding="utf-8")
 
-st.components.v1.html(
+html(
     html_content,
-    height=900,
+    width=1920,
+    height=4000,
     scrolling=True
 )
